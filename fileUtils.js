43a1e4db0d5f206756b441e8b1528b6097f4ab19
Paola Andrea Ramirez <paolandre38@gmail.com>
2023-07-27 00:42:30 -0500

    Ya muestra el estatus y los otros datos de la petición, pero no se muestran junto con los anteriroes datos

--- fileUtils.js
+++ fileUtils.js
@@ -4,6 +4,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 import path from 'path';
+import axios from 'axios';
 
 //  Función para obtener los archivos de un directorio
 export const getMdFilesInDirectories = (directory) => {
@@ -70,3 +71,40 @@ export const mdFile = (absolutePath) => {
   console.log(chalk.inverse.magenta('#30'));
   return isAmdFile;
 };
+
+// Función validate
+// Declaramos la función validateLink que toma un parámetro llamado 'link'
+export function validateLink(link) {
+  // Retornamos una nueva Promesa. Las Promesas son objetos que representan el resultado eventual
+  // de una operación asíncrona. Esta Promesa recibe una función como argumento que tiene dos parámetros:
+  // 'resolve' y 'reject', que son funciones que se llamarán cuando la Promesa se resuelva o rechace respectivamente.
+  return new Promise((resolve, reject) => {
+    // Hacemos una petición HTTP GET al 'link' usando axios. Axios es una biblioteca de JavaScript
+    // que nos permite hacer peticiones HTTP fácilmente. Cuando la petición se complete, entonces se llamará
+    // a la función que pasamos al método 'then'.
+    axios.get(link)
+      .then((response) => {
+        // Cuando la petición se completa exitosamente, se llama a esta función.
+        // 'response' es un objeto que contiene información sobre la respuesta HTTP,
+        // incluyendo el estado (response.status).
+        // Resolvemos la promesa con un objeto que contiene el 'link' (href), el estado de la respuesta HTTP
+        // y un campo 'ok' que será 'ok' si el estado es HttpStatus.OK, y 'fail' en caso contrario.
+        resolve({
+          href: link,
+          status: response.status,
+          ok: response.status === 200 ? 'ok' : 'fail',
+        });
+      })
+      .catch((error) => {
+        // Si hubo algún error en la petición (como una falla de red, o si el servidor retorna un error),
+        // entonces se llama a esta función. 'error' es un objeto que contiene información sobre el error.
+        // Nota que resolvemos la promesa incluso en caso de error. Esto es porque aún en caso de error,
+        // queremos retornar un resultado que indique el 'link', un estado 'ERROR', y 'ok' como 'fail'.
+        resolve({
+          href: link,
+          status: error.response ? error.response.status : 'ERROR',
+          ok: 'fail',
+        });
+      });
+  });
+}
