ffb140988d2f6c29adee58af5edb2ea33912475e
Paola Andrea Ramirez <paolandre38@gmail.com>
2023-07-31 16:27:33 -0500

    Función validate terminada

--- fileUtils.js
+++ fileUtils.js
@@ -4,7 +4,6 @@
 import fs from 'fs';
 import chalk from 'chalk';
 import path from 'path';
-import axios from 'axios';
 
 //  Función para obtener los archivos de un directorio
 export const getMdFilesInDirectories = (directory) => {
@@ -12,7 +11,7 @@ export const getMdFilesInDirectories = (directory) => {
 
   // Función recursiva para explorar los archivos y directorios dentro del  directorio
   const inCurrentDirectory = (currentDirectory) => {
-    console.log(chalk.inverse.magenta('#12'));
+    console.log(chalk.inverse.magenta('entra a la función recursiva'));
     // Obtiene la lista de archivos en el directorio actual
     const files = fs.readdirSync(currentDirectory);
     // Itera sobre los archivos
@@ -21,7 +20,6 @@ export const getMdFilesInDirectories = (directory) => {
       const filePath = path.join(currentDirectory, file);
       // Si es un directorio
       if (fs.statSync(filePath).isDirectory()) {
-        console.log(chalk.inverse.magenta('#43'));
         // se llama recursivamente a la función inDirectory
         inCurrentDirectory(filePath);
       } else if (path.extname(file) === '.md') {
@@ -34,7 +32,7 @@ export const getMdFilesInDirectories = (directory) => {
 
   // Llamada inicial a la función inDirectory
   inCurrentDirectory(directory);
-  console.log(chalk.inverse.magenta('#14'));
+  console.log(chalk.inverse.magenta('Llama a la función recursiva'));
   return filesMD;
 };
 
@@ -43,7 +41,7 @@ export const convertToAbsolutePath = (route) => {
   if (!path.isAbsolute(route)) {
     // Convierte la ruta en absoluta y la asigna a absolutePath
     const absolutePath = path.resolve(route);
-    console.log(chalk.inverse.magenta('#20'));
+    console.log(chalk.inverse.magenta('Convierte a ruta absoluta'));
     return absolutePath;
   }
   return route; // Si ya es una ruta absoluta, retorna la ruta sin modificar
@@ -54,7 +52,7 @@ export const arrayFile = (absolutePath) => {
   let mdFileArray = [];
   if (path.extname(absolutePath) === '.md') {
     mdFileArray = [absolutePath];
-    console.log(chalk.inverse.magenta('#13'));
+    console.log(chalk.inverse.magenta('Almacena el archivo en un array'));
   }
   return (mdFileArray);
 };
@@ -68,43 +66,6 @@ export const directory = (absolutePath) => {
 // Verificar si es un archivo .md
 export const mdFile = (absolutePath) => {
   const isAmdFile = path.extname(absolutePath) === '.md';
-  console.log(chalk.inverse.magenta('#30'));
+  console.log(chalk.inverse.magenta('Es un archivo md'));
   return isAmdFile;
 };
-
-// Función validate
-// Declaramos la función validateLink que toma un parámetro llamado 'link'
-export function validateLink(link) {
-  // Retornamos una nueva Promesa. Las Promesas son objetos que representan el resultado eventual
-  // de una operación asíncrona. Esta Promesa recibe una función como argumento que tiene dos parámetros:
-  // 'resolve' y 'reject', que son funciones que se llamarán cuando la Promesa se resuelva o rechace respectivamente.
-  return new Promise((resolve, reject) => {
-    // Hacemos una petición HTTP GET al 'link' usando axios. Axios es una biblioteca de JavaScript
-    // que nos permite hacer peticiones HTTP fácilmente. Cuando la petición se complete, entonces se llamará
-    // a la función que pasamos al método 'then'.
-    axios.get(link)
-      .then((response) => {
-        // Cuando la petición se completa exitosamente, se llama a esta función.
-        // 'response' es un objeto que contiene información sobre la respuesta HTTP,
-        // incluyendo el estado (response.status).
-        // Resolvemos la promesa con un objeto que contiene el 'link' (href), el estado de la respuesta HTTP
-        // y un campo 'ok' que será 'ok' si el estado es HttpStatus.OK, y 'fail' en caso contrario.
-        resolve({
-          href: link,
-          status: response.status,
-          ok: response.status === 200 ? 'ok' : 'fail',
-        });
-      })
-      .catch((error) => {
-        // Si hubo algún error en la petición (como una falla de red, o si el servidor retorna un error),
-        // entonces se llama a esta función. 'error' es un objeto que contiene información sobre el error.
-        // Nota que resolvemos la promesa incluso en caso de error. Esto es porque aún en caso de error,
-        // queremos retornar un resultado que indique el 'link', un estado 'ERROR', y 'ok' como 'fail'.
-        resolve({
-          href: link,
-          status: error.response ? error.response.status : 'ERROR',
-          ok: 'fail',
-        });
-      });
-  });
-}
--- mdLinks.js
+++ mdLinks.js
@@ -9,81 +9,67 @@ import {
   arrayFile,
   directory,
   mdFile,
-  validateLink,
 } from './fileUtils.js';
 import { readMdFiles, getLinks } from './mdLinkExtractor.js';
+import validateLink from './validate.js';
 
 const mdLinks = (route, options) => new Promise((resolve, reject) => {
   if (!fs.existsSync(route)) {
     reject(Error('La ruta no existe'));
-    console.log(chalk.inverse.magenta('#11'));
+    console.log(chalk.inverse.magenta('La ruta existe'));
     return;
   }
-
   const absolutePath = convertToAbsolutePath(route);
   const mdFileArray = arrayFile(absolutePath);
   const isMdFile = mdFile(absolutePath);
   const isDir = directory(absolutePath);
-
-  if (isMdFile) {
-    const contentMdFile = readMdFiles(mdFileArray);
-    const linksMd = getLinks(contentMdFile, mdFileArray);
-    const formattedLinks = linksMd.map((link) => {
-      const text = chalk.yellow(`Text: ${link.text}`);
-      const url = chalk.cyan(`URL: ${link.url}`);
-      const file = chalk.greenBright(`File: ${link.file}`);
-      return `${text}\n${url}\n${file}\n\n`;
-    });
-
-    resolve(formattedLinks);
-    return;
-  }
-
-  // Si no es un archivo ni un directorio
+  let linksMds = [];
+  let mdFiles = []; // Declara la variable fuera del bloque if.
   if (!isMdFile && !isDir) {
     reject(Error('No es un archivo .md ni un directorio'));
-    console.log(chalk.inverse.magenta('#15'));
+    console.log(chalk.inverse.magenta('No es directorio ni archivo'));
     return;
   }
-
-  // Si es un directorio
   if (isDir) {
-    console.log(chalk.inverse.magenta('#2'));
-    // Obtiene los archivos .md en el directorio
-    const mdFiles = getMdFilesInDirectories(absolutePath);
-    // Si no se encontraron archivos .md en el directorio, rechaza la promesa
-    if (mdFiles.length === 0) {
-      reject(Error('No se encontraron archivos .md en el directorio'));
-      console.log(chalk.inverse.magenta('#5'));
-      return;
-    }
-    // Resuelve la promesa con el array de archivos .md
-    if (mdFiles.length > 0) {
-      const contentMdFiles = readMdFiles(mdFiles);
-      const linksMds = getLinks(contentMdFiles, mdFiles);
-      const formattedLinks = linksMds.map((link) => {
-        const text = chalk.cyan(`Text: ${link.text}`);
-        const url = chalk.magenta(`URL: ${link.url}`);
-        const file = chalk.blue(`File: ${link.file}`);
-        return `${text}\n${url}\n${file}\n\n`;
-      });
-
-      if (options && options.validate) {
-        // Usamos Promise.all para validar todos los links al mismo tiempo
-        Promise.all(linksMds.map((link) => validateLink(link.url)))
-          .then((validatedLinks) => {
-            // Una vez que todos los links han sido validados, los resolvemos
-            resolve(validatedLinks);
-          })
-          .catch((error) => {
-            // Si ocurre algún error durante la validación, lo rechazamos
-            reject(error);
-          });
-      }
-
-      resolve(formattedLinks);
-    }
+    console.log(chalk.inverse.magenta('Es directorio'));
+    mdFiles = getMdFilesInDirectories(absolutePath); // Asigna el valor aquí.
+  } else if (isMdFile) {
+    mdFiles.push(absolutePath);
   }
+  // Si no se encontraron archivos .md en el directorio, rechaza la promesa
+  if (mdFiles.length === 0) {
+    reject(Error('No hay arhivos .md'));
+    console.log(chalk.inverse.magenta('No hay archivos md'));
+  }
+  // Verifica si mdFiles está definida antes de usarla.
+  if (mdFiles && mdFiles.length > 0) {
+    const contentMdFiles = readMdFiles(mdFiles);
+    linksMds = getLinks(contentMdFiles, mdFiles);
+  }
+
+  if (options && options.validate) {
+    // Usamos Promise.all para validar todos los links al mismo tiempo
+    Promise.all(linksMds.map((singleLink) => validateLink(singleLink.url)))
+      .then((validatedLinks) => {
+        // Una vez que todos los links han sido validados, los resolvemos
+        const formattedLinks = validatedLinks.map((validatedLink, index) => {
+          const text = chalk.cyan(`Texto: ${linksMds[index].text}`);
+          const url = chalk.magenta(`URL: ${linksMds[index].url}`);
+          const file = chalk.blue(`Archivo: ${linksMds[index].file}`);
+          const href = chalk.yellow(`Href: ${validatedLink.href}`);
+          const status = chalk.greenBright(`Estado: ${validatedLink.status}`);
+          const ok = chalk.cyan(`Ok: ${validatedLink.ok}`);
+          return `${text}\n${url}\n${file}\n${href}\n${status}\n${ok}\n\n`;
+        });
+        resolve(formattedLinks);
+      })
+      .catch((error) => {
+        // Si ocurre algún error durante la validación, lo rechazamos
+        reject(error);
+      });
+    return;
+  }
+  resolve(linksMds);
 });
 
 //  RUTAS
@@ -97,7 +83,7 @@ const rutaNoExiste = 'NoExiste.md';
 const rutaUnaCarpetaConArchivo = 'Directorio Uno/Directorio Tres';
 const noLinks = 'Directorio Uno/Directorio Tres/sinlinks.md';
 
-mdLinks(rutaAbsolutaDirectorioDos, { validate: true })
+mdLinks(noLinks, { validate: true })
   .then((rutaAbsoluta) => {
     console.log(chalk.cyan(rutaAbsoluta));
   })
